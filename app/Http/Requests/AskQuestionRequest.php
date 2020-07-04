e449cd82b8fe9df7fffd6d6860048102784a7474
Aleksandar Ardjanliev <sasce1974@yahoo.com>
2020-07-04 22:05:34 +0200

    Store question method completed

---
+++ app/Http/Requests/AskQuestionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AskQuestionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; //true means all users can use this request
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'=> 'required|max:255',
+            'body'=> 'required'
+        ];
+    }
+}
