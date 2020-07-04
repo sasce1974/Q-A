fce2177aeff69e32c75d850cfca2f48413f49e53
Aleksandar Ardjanliev <sasce1974@yahoo.com>
2020-07-04 17:17:36 +0200

    Created User, Question model, controller and view, faker

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function (){
+    $r = \App\Question::where('id', 4)->first();
+    dd($r);
+});
+
+Route::resource('questions', 'QuestionsController');
