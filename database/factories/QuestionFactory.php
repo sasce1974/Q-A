44c341ba6c2b18532078fd5849f6b5e202909ff2
Aleksandar Ardjanliev <sasce1974@yahoo.com>
2020-07-05 18:35:06 +0200

    Generate fake answers

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -10,7 +10,7 @@ $factory->define(App\Question::class, function (Faker $faker) {
         'title'=> rtrim($faker->sentence(rand(5, 10)), "."),
         'body'=> $faker->paragraphs(rand(3, 7), true),
         'views'=> rand(0, 10),
-        'answers_count'=> rand(0, 10),
+        //'answers_count'=> rand(0, 10), //now it is handled by Answer static method boot
         'votes'=> rand(-3, 10),
     ];
 });
